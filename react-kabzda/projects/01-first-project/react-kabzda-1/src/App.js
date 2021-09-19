c88a220dadacd65ffa84c2d036887b72c90c7f87
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2021-09-19 09:32:30 +0300

    no message

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/App.js
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/App.js
@@ -8,6 +8,7 @@ import Profile from './Components/Profile/Profile';
 const App = () => {
   return (
     <div className='app-wrapper'>
+          <img src='https://d1csarkz8obe9u.cloudfront.net/posterpreviews/app-logo-design-template-5ceda3418eba54b040875a86d7e7b8ec_screen.jpg?ts=1573502884'/>
       <Header />
       <Navbar />
       <Profile />
