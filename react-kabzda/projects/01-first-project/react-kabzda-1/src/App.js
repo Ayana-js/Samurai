0e74b8a29cd8aeff65d1955f265d1d1763fc742b
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2021-09-20 20:07:52 +0300

    22. Добавлены Route маршрутизаторы, дополняю компонент Message

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/App.js
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/App.js
@@ -4,19 +4,30 @@ import Dialogs from './Components/Dialogs/Dialogs';
 import Header from './Components/Header/Header'
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
+import Settings from './Components/Settings/Settings';
+import News from './Components/News/News';
+import Music from './Components/Music/Music';
+import {BrowserRouter, Route} from "react-router-dom";
+
+
 
 
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
       <Header />
       <Navbar />
       <div className='app-wrapper-content'>
-        <Dialogs />
+        <Route path='/dialogs' component={Dialogs}/>
+        <Route path='/music' component={Music}/>
+        <Route path='/settings' component={Settings}/>
+        <Route path='/news' component={News}/>
+        <Route path='/profile' component={Profile}/>
       </div>
-      {/* <Profile /> */}
     </div>
+    </BrowserRouter>
   );
 }
 
--- react-kabzda/projects/01-first-project/react-kabzda-1/src/Components/Dialogs/Dialogs.jsx
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/Components/Dialogs/Dialogs.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1'>  Ayana </NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>Aizhan </NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'>Tom Hardy </NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3'>Sam Claflin </NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'>Chris Evans</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/5'>Jason Momoa</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi, I'm Ayana
+                </div>
+                <div className={s.message}>
+                    Hello
+                </div>
+                <div className={s.message}>
+                    Yo, you're both so beautiful
+                </div>
+            </div>
         </div>
     )
 }
